a6db17e4c5a478b3eaadb8b9a09709922ea5e9d6
bsjin1122 <kitty7579@hanmail.net>
2023-03-22 12:55:02 +0900

    class 변수 사용

---
+++ MyOOP.java
@@ -0,0 +1,28 @@
+public class MyOOP {
+
+    public static String delimiter = "";
+    public static void main(String[] args) {
+        delimiter = "----";
+        printA();
+        printA();
+        printB();
+        printB();
+
+        delimiter = "****";
+        printA();
+        printA();
+        printB();
+        printB();
+    }
+
+    private static void printA() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+    private static void printB() {
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
+    }
+}
