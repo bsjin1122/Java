66b4cc712ccb55a7a4d16f9e3def2db71fa7231c
bsjin1122 <kitty7579@hanmail.net>
2023-03-24 08:58:20 +0900

    생활코딩 객체지향 - 인스턴스화

--- MyOOP3.java
+++ MyOOP3.java
@@ -1,14 +1,13 @@
 public class MyOOP3 {
     public static void main(String[] args) {
-        Print p1 = new Print();
-        p1.delimiter = "----";
+        Print p1 = new Print("----");
+        System.out.println(2);
         p1.A();
         p1.A();
         p1.B();
         p1.B();
 
-        Print p2 = new Print();
-        p2.delimiter = "****";
+        Print p2 = new Print("****");
         p2.A();
         p2.A();
         p2.B();
