93669950127e4076ddfb348c48435da25ea1fbdf
SEOJIN <kitty7579@hanmail.net>
2023-10-27 00:42:53 +0900

    LinkedList 생활코딩 학습 addLast, node, add, toString, removeFirst

--- src/DataStructure/LinkedList/implementation/LinkedList.java
+++ src/DataStructure/LinkedList/implementation/LinkedList.java
@@ -6,7 +6,7 @@ public class LinkedList {
     private Node tail;
     private int size = 0;
 
-    public void addFirst(int input) {
+    public void addFirst(Object input) {
         Node newNode = new Node(input);
         newNode.next = head;
         head = newNode;
@@ -15,7 +15,6 @@ public class LinkedList {
             tail = head;
         }
     }
-
     // 하나의 element가 하나의 객체
     // element == node == vertex
     private class Node{
@@ -29,4 +28,90 @@ public class LinkedList {
             return String.valueOf(this.data);
         }
     }
+
+    // 28강 addLast
+    public void addLast(Object input){
+        Node newNode = new Node(input);
+        if(size == 0){
+            addFirst(input);
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+            size++;
+        }
+    }
+
+    // 29강 Node
+    // public 노출시켜서는 안되는 메서드
+    Node node(int index){
+        Node x = head;
+        for(int i = 0; i < index; i++){
+            x = x.next;
+        }
+        //x = x.next;
+        //x = x.next;
+        return x;
+    }
+
+    public void add(int k, Object input){
+        if(k == 0){
+            addFirst(input);
+        }else{
+            Node temp1 = node(k-1);
+            Node temp2 = temp1.next;
+            Node newNode = new Node(input);
+            temp1.next = newNode;
+            newNode.next = temp2;
+            size++;
+            if(newNode.next == null){
+                tail = newNode;
+            }
+        }
+    }
+
+    // toString()
+    public String toString(){
+        if(head == null){
+            return "[]";
+        }
+        String str = "[";
+        Node temp = head;
+        while(temp.next != null){
+            str += temp.data + ", ";
+            temp = temp.next;
+
+        }
+        str += temp.data;
+
+        /*for(int i=0; i < size; i++){
+            str += (Object) node(i);
+            if(i < size-1){
+                str += ",";
+            }
+        }*/
+        return str + "]";
+    }
+
+    // removeFirst
+    // remove했을 때 삭제된 노드가 갖고 있던 값을 return
+    /* 내가 시도해본 방식
+    public boolean removeFirst(){
+        if(size == 0){
+            return true; //삭제할 것이 없음
+        }else{
+            head = head.next;
+            size--;
+            return true;
+        }
+    }*/
+
+    public Object removeFirst(){
+        Node temp = head;
+        head = head.next;
+        Object returnData = temp.data;
+        temp = null;
+        size--;
+        return returnData;
+    }
+
 }
